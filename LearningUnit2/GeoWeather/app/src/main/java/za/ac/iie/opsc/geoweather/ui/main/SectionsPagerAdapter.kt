3284750207a7050a98f292277782ad38802c2fd3
Hermien Pellissier <miena.moo@gmail.com>
2023-03-05 21:24:03 +0200

    Incorporating the 5-day forecast and improving look and feel.

--- LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/ui/main/SectionsPagerAdapter.kt
+++ LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/ui/main/SectionsPagerAdapter.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import za.ac.iie.opsc.geoweather.DailyForecastsFragment
 import za.ac.iie.opsc.geoweather.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 /**
@@ -20,7 +22,10 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
 
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
+        when (position) {
+            1 -> return DailyForecastsFragment()
+        }
+        // Return a PlaceholderFragment.
         return PlaceholderFragment.newInstance(position + 1)
     }
 
@@ -29,7 +34,7 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        // Show 3 total pages.
+        return 3
     }
 }
