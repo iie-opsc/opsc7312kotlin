9150a85c23aa1dd3e3e93abf0652ff1ec9afb2e7
Hermien Pellissier <miena.moo@gmail.com>
2023-03-05 13:56:22 +0200

    Created the new project.

---
+++ BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package za.ac.iie.opsc.basicweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
