44b4fa05b7b8a3745d4228004e98c5ba8a42dce5
Hermien Pellissier <miena.moo@gmail.com>
2023-03-05 15:11:46 +0200

    Added AccuWeather branding according to requirements

--- BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/MainActivity.kt
+++ BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/MainActivity.kt
@@ -1,5 +1,8 @@
 package za.ac.iie.opsc.basicweatherapp
 
+import android.R
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import za.ac.iie.opsc.basicweatherapp.databinding.ActivityMainBinding
@@ -14,6 +17,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // add an event handler to open the Accu Weather website
+        binding.ivAccuweather.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("http://www.accuweather.com/")
+            )
+            startActivity(intent)
+        }
 
         // Thanks to https://stackoverflow.com/questions/46177133/http-request-in-android-with-kotlin
         thread {
