15f2417c1ec1eed8c0cca5cc75dc56fac3724214
Hermien Pellissier <miena.moo@gmail.com>
2023-03-05 14:57:32 +0200

    Building the URL to access weather data, reading the data and displaying it in a raw format.

--- BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/MainActivity.kt
+++ BasicWeatherApp/app/src/main/java/za/ac/iie/opsc/basicweatherapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package za.ac.iie.opsc.basicweatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import za.ac.iie.opsc.basicweatherapp.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Thanks to https://stackoverflow.com/questions/46177133/http-request-in-android-with-kotlin
+        thread {
+            val weather = try {
+                buildURLForWeather()?.readText()
+            } catch (e: Exception) {
+                return@thread
+            }
+            runOnUiThread { binding.tvWeather.text = weather }
+        }
     }
 }
