26776eae375ff8ca1a1d29af1a0d1436ae2591a4
Hermien Pellissier <miena.moo@gmail.com>
2023-04-01 22:31:13 +0200

    Getting current location

--- LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/MainActivity.kt
+++ LearningUnit2/GeoWeather/app/src/main/java/za/ac/iie/opsc/geoweather/MainActivity.kt
@@ -1,19 +1,28 @@
 package za.ac.iie.opsc.geoweather
 
+import android.R
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.viewpager.widget.ViewPager
+import com.google.android.gms.location.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import za.ac.iie.opsc.geoweather.ui.main.SectionsPagerAdapter
 import za.ac.iie.opsc.geoweather.databinding.ActivityMainBinding
+import za.ac.iie.opsc.geoweather.ui.main.SectionsPagerAdapter
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +30,105 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+        checkPermissionsAndRequestLocation();
+
         val fab: FloatingActionButton = binding.fab
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Replace with your own action",
+                Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+    }
+
+    private fun checkPermissionsAndRequestLocation() {
+        val hasFineLocationPermission = ActivityCompat.checkSelfPermission(
+            this, "android.permission.ACCESS_FINE_LOCATION"
+        )
+        val hasCourseLocationPermission = ActivityCompat.checkSelfPermission(
+            this, "android.permission.ACCESS_COARSE_LOCATION"
+        )
+        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED &&
+            hasCourseLocationPermission != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissions = arrayOf<String>(
+                "android.permission.ACCESS_FINE_LOCATION",
+                "android.permission.ACCESS_COARSE_LOCATION"
+            )
+            // Request permission - this is asynchronous
+            ActivityCompat.requestPermissions(this, permissions, 0)
+        } else {
+            // We have permission, so now ask for the location
+            getLocationAndCreateUI()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(
+            requestCode, permissions,
+            grantResults
+        )
+        // is this for our request?
+        if (requestCode == 0) {
+            if (grantResults.size > 0 &&
+                (grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED ||
+                        grantResults[1] ==
+                        PackageManager.PERMISSION_GRANTED)
+            ) {
+                getLocationAndCreateUI()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Location permission denied",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocationAndCreateUI() {
+        val locationRequest: LocationRequest = buildLocationRequest()
+        val locationCallback: LocationCallback = buildLocationCallBack()
+        fusedLocationProviderClient!!.requestLocationUpdates(
+            locationRequest,
+            locationCallback, Looper.myLooper()
+        )
+    }
+
+    private fun buildLocationRequest(): LocationRequest {
+        val locationRequest = LocationRequest()
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        locationRequest.setInterval(5000)
+        locationRequest.setFastestInterval(3000)
+        locationRequest.setSmallestDisplacement(10.0f)
+        return locationRequest
+    }
+
+    private fun buildLocationCallBack(): LocationCallback {
+        return object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                val location: Location = locationResult.lastLocation
+                Log.i("LocationResult", "onLocationResult: $location")
+
+                // TODO: Get the locationKey from AccuWeater
+                val sectionsPagerAdapter = SectionsPagerAdapter(
+                    this@MainActivity,
+                    supportFragmentManager
+                )
+                val viewPager: ViewPager = binding.viewPager
+                viewPager.adapter = sectionsPagerAdapter
+                val tabs: TabLayout = binding.tabs
+                tabs.setupWithViewPager(viewPager)
+            }
         }
     }
 }
