524a66f81e82bc98cd9d3e6dba60316a230f7408
Hermien Pellissier <miena.moo@gmail.com>
2023-04-05 18:49:40 +0200

    Basic user interface implemented

---
+++ LearningUnit3/PhotoMemories/app/src/main/java/za/ac/iie/opsc/photomemories/LocalImagesActivity.kt
@@ -0,0 +1,11 @@
+package za.ac.iie.opsc.photomemories
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocalImagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_local_images)
+    }
+}
---
+++ LearningUnit3/PhotoMemories/app/src/main/java/za/ac/iie/opsc/photomemories/MainChoiceFragment.kt
@@ -0,0 +1,76 @@
+package za.ac.iie.opsc.photomemories
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainChoiceFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainChoiceFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_main_choice,
+                                    container, false)
+        val btn_local: Button = view.findViewById(R.id.btn_local)
+        btn_local.setOnClickListener {
+            val openLocalImages = Intent(activity,
+                LocalImagesActivity::class.java)
+            startActivity(openLocalImages)
+        }
+
+        val btn_cloud: Button = view.findViewById(R.id.btn_cloud)
+        btn_cloud.setOnClickListener {
+//            val openCloudImages = Intent(activity, CloudImagesActivity::class.java)
+//            startActivity(openCloudImages)
+        }
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MainChoiceFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainChoiceFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
